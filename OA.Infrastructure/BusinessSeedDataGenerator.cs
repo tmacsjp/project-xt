b3cb28ff2491dc3fdacef396420502dc126acf60
tmac <15958176015@163.com>
2022-11-16 14:18:10 +0800

    x

---
+++ OA.Infrastructure/BusinessSeedDataGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Infrastructure
+{
+    public class BusinessSeedDataGenerator: ISeedDataGenerator
+    {
+        BusinessDbContext _dbContext;
+        public BusinessSeedDataGenerator(BusinessDbContext businessDbContext)
+        {
+            _dbContext = businessDbContext;
+        }
+        public async Task Execute()
+        {
+            var sql = _dbContext.Database.GenerateCreateScript();
+            int k = 0;
+
+
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+    public interface ISeedDataGenerator
+    {
+        Task Execute();
+    }
+
+}
---
+++ OA.Infrastructure/SeedDataGenerateExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Infrastructure
+{
+    public static class SeedDataGenerateExtensions
+    {
+        public static void GenerateSeedData(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var generators = scope.ServiceProvider.GetServices<ISeedDataGenerator>();
+            foreach (var a in generators)
+            {
+                a.Execute().Wait();
+            }
+        }
+
+    }
+}
